83377b8acac06c2a96ef24dc33ac3d878f93a38a
Izzeldeen.Ashour <Izzeldeen.Ashour@b12app.com>
2024-05-02 13:28:50 +0300

    push

--- src/pages/services/[id].jsx
+++ src/pages/services/[id].jsx
@@ -3,12 +3,10 @@ import PageBanner from "@/src/components/PageBanner";
 import { useEffect } from "react";
 import { useLocale } from "@/utils/getLocale";
 import { Accordion } from "../../common/utilits";
-import Link from "next/link";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import PricingSection from "@components/sections/Pricing";
 import { useRouter } from "next/router"; 
 import servicesData from "@data/services/services.json"
-const ServiceDetail = ( { data, related } ) => {
+const ServiceDetail = () => {
     const {activeLocale,t} = useLocale();
     const router = useRouter();
     const {id} = router.query;
@@ -32,34 +30,37 @@ const ServiceDetail = ( { data, related } ) => {
       <section id="service">
           <div className="container mil-p-120-90">
               <div className="row justify-content-between">
-              <div className={ `${postData.list && postData.list.items && postData.list.items.length > 0 ? "col-lg-4" :""} mil-relative mil-mb-90`}>
+              <div className={ `${postData.list.items[0].label.english!=='' ? "col-lg-4" :""} mil-relative mil-mb-90`}>
 
-                      <h4 className="mil-up mil-mb-30" dangerouslySetInnerHTML={{__html : activeLocale ==='ar' ? postData.description.title.arabic : postData.description.title.english}} />
-                      <p className="mil-up mil-mb-30" dangerouslySetInnerHTML={{__html :activeLocale==='ar' ? postData.description.content.arabic : postData.description.content.english}} />
+                      <h4 className="mil-up mil-mb-30"  >
+                        {activeLocale ==='ar' ? postData.description.title.arabic : postData.description.title.english}
+                      </h4>
+                      <p className="mil-up mil-mb-30"  >
+                      {activeLocale==='ar' ? postData.description.content.arabic : postData.description.content.english}
+                      </p>
                       {/* <div className="mil-up">
                           <Link href={postData.description.button.link} className="mil-link mil-dark mil-arrow-place">
-                              <span>{postData.description.button.label}</span>
+span>{postD                              <ata.description.button.label}</span>
                           </Link>
                       </div> */}
 
                   </div>
                   <div className="col-lg-6">
-                  {postData.list != undefined && postData.list.items.length != 0 &&
+                  {postData.list != undefined && postData.list.items[0].label.english!=='' &&
                   <>
                       {postData.list.items.map((item, key) => (
-                      <div className="mil-accordion-group mil-up" key={`service-list-${key}`}>
-                          <div className="mil-accordion-menu">
-
-                              <p className="mil-accordion-head">{ activeLocale ==='ar' ?  item.label.arabic : item.label.english}</p>
-
-                              <div className="mil-symbol mil-h3 ">
-                                  <div className="mil-plus">+</div>
-                                  <div className="mil-minus">-</div>
-                              </div>
-
-                          </div>
-                          <div className="mil-accordion-content mil-text" dangerouslySetInnerHTML={{__html : activeLocale ==='ar' ? item.value.arabic : item.value.english}} />
-                      </div>
+                <div className="mil-accordion-group mil-up" key={`service-list-${key}`}>
+                    <div className="mil-accordion-menu">
+                        <p className="mil-accordion-head">{activeLocale === 'ar' ? item.label.arabic : item.label.english}</p>
+                        <div className="mil-symbol mil-h3 ">
+                            <div className="mil-plus">+</div>
+                            <div className="mil-minus">-</div>
+                        </div>
+                    </div>
+                    <div className="mil-accordion-content mil-text"  >
+                      {activeLocale === 'ar' ? item.value.arabic : item.value.english}
+                    </div>
+                </div>
                       ))}
                   </>
                   }
