3d947fb98d9b37a1d4cfe22459ff2debeb43c2fc
Izzeldeen Ashour <ezraashour00@gmail.com>
2024-05-06 21:27:51 +0300

    push

--- src/layouts/svg-icons/LightEnglishLogo.jsx
+++ src/layouts/svg-icons/LightEnglishLogo.jsx
@@ -1,12 +1,17 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 const LightEnglishLogo = ({url}) => {
+
   return (
-    <Link href="/" >
-       <Image src={url} priority={true}  alt="Eng-logo"  width={55} 
-          height={40}  /> 
-    </Link>
+    <div >
+    <Image src={url} priority={true}  alt="Eng-logo"  width={55} height={40} /> 
+  </div>
+    // <Link onClick={handleLogoClick}>
+    //    <Image src={url} priority={true}  alt="Eng-logo"  width={55} 
+    //       height={40}  /> 
+    // </Link>
   )
 }
 
