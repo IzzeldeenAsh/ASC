e6ba6157dcc9e4ece27f9464beb69d2b20239519
Izzeldeen.Ashour <Izzeldeen.Ashour@b12app.com>
2024-07-09 14:43:34 +0300

    push

--- src/components/Hover-cards/CardGrid.js
+++ src/components/Hover-cards/CardGrid.js
@@ -4,27 +4,35 @@ const CardGrid = () => {
 
   return (
     <>
-      <section className="cards mil-p-60-0">
+    <div className=' mil-p-60-0 '>
+      <section className="cards ">
         <article className="card card--1">
           <div className="card__info">
             <h3 className="card__title">Smart Cities and Mobility</h3>
             <div className="card__description">
             We leverage key technologies and smart solutions to navigate the complexity of...
             </div>
+            <div className="card__description__hover">
+            We leverage key technologies and smart solutions to navigate the complexity of smart city and mobility development. As ITS & MaaS, AI and IoT have a lot to offer, we guide our clients in navigating the complexity of this ecosystem.
+            </div>
           </div>
           <div className="card__img"></div>
           <a href="#" className="card_link">
             <div className="card__img--hover"></div>
           </a>
-
+     
         </article>
 
         <article className="card card--2">
           <div className="card__info">
-            <h3 className="card__title">Discover the sea</h3>
+            <h3 className="card__title">Digital Transformation</h3>
             <div className="card__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Adopting the right digital technologies and integrating them into the various...
             </div>
+            <div className="card__description__hover">
+            Adopting the right digital technologies and integrating them into the various aspects of an organization's operations, culture, and customer experience has become essential for businesses to remain competitive.
+            </div>
+           
           </div>
           <div className="card__img"></div>
           <a href="#" className="card_link">
@@ -35,13 +43,14 @@ const CardGrid = () => {
 
         <article className="card card--3">
           <div className="card__info">
-            <h3 className="card__title">Discover the sea</h3>
+            <h3 className="card__title">Renewable Energy</h3>
             <div className="card__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            A&B team of experts is dedicated to helping businesses ...
             </div>
-            <div className="card__description">
-              ReadMore
+            <div className="card__description__hover">
+            A&B team of experts is dedicated to helping businesses and organizations lower their energy costs and reduce their carbon footprint.
             </div>
+           
           </div>
           <div className="card__img"></div>
           <a href="#" className="card_link">
@@ -49,8 +58,80 @@ const CardGrid = () => {
           </a>
 
         </article>
+         <article className="card card--4 up-1550px">
+          <div className="card__info">
+            <h3 className="card__title">Renewable Energy</h3>
+            <div className="card__description">
+            A&B team of experts is dedicated to helping businesses ...
+            </div>
+            <div className="card__description__hover">
+            A&B team of experts is dedicated to helping businesses and organizations lower their energy costs and reduce their carbon footprint.
+            </div>
+           
+          </div>
+          <div className="card__img"></div>
+          <a href="#" className="card_link">
+            <div className="card__img--hover"></div>
+          </a>
+
+        </article>
+        <article className="card card--5 up-1550px">
+          <div className="card__info">
+            <h3 className="card__title">Renewable Energy</h3>
+            <div className="card__description">
+            A&B team of experts is dedicated to helping businesses ...
+            </div>
+            <div className="card__description__hover">
+            A&B team of experts is dedicated to helping businesses and organizations lower their energy costs and reduce their carbon footprint.
+            </div>
+           
+          </div>
+          <div className="card__img"></div>
+          <a href="#" className="card_link">
+            <div className="card__img--hover"></div>
+          </a>
+
+        </article> 
+      </section>
+      <section className="cards-2 mil-p-30-30 down-1550px" >
+   
+       <article className="card card--4">
+          <div className="card__info">
+            <h3 className="card__title">Renewable Energy</h3>
+            <div className="card__description">
+            A&B team of experts is dedicated to helping businesses ...
+            </div>
+            <div className="card__description__hover">
+            A&B team of experts is dedicated to helping businesses and organizations lower their energy costs and reduce their carbon footprint.
+            </div>
+           
+          </div>
+          <div className="card__img"></div>
+          <a href="#" className="card_link">
+            <div className="card__img--hover"></div>
+          </a>
+
+        </article>
+        <article className="card card--5">
+          <div className="card__info">
+            <h3 className="card__title">Renewable Energy</h3>
+            <div className="card__description">
+            A&B team of experts is dedicated to helping businesses ...
+            </div>
+            <div className="card__description__hover">
+            A&B team of experts is dedicated to helping businesses and organizations lower their energy costs and reduce their carbon footprint.
+            </div>
+           
+          </div>
+          <div className="card__img"></div>
+          <a href="#" className="card_link">
+            <div className="card__img--hover"></div>
+          </a>
+
+        </article> 
       </section>
 
+  </div>
     
     </>
   );
